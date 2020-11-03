9df2c757ce8236f5dedb67fdc002e548ce531198
kiranyng <kiran.yng@gmail.com>
2020-11-03 18:16:50 +0530

    Updates

---
+++ precache-manifest.103a4d4df8f964ff011da4b314f7c024.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7765ae762813ede839ee9f43ae0b8b7f",
+    "url": "/trackify/index.html"
+  },
+  {
+    "revision": "960cfb11478d367b8155",
+    "url": "/trackify/static/css/2.515377c4.chunk.css"
+  },
+  {
+    "revision": "bcf2badc8b315aff47a4",
+    "url": "/trackify/static/css/main.1b22cea2.chunk.css"
+  },
+  {
+    "revision": "960cfb11478d367b8155",
+    "url": "/trackify/static/js/2.94bc173c.chunk.js"
+  },
+  {
+    "revision": "fcc113bba81180185718b6c5aa3a2670",
+    "url": "/trackify/static/js/2.94bc173c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf2badc8b315aff47a4",
+    "url": "/trackify/static/js/main.106a3ea6.chunk.js"
+  },
+  {
+    "revision": "7974796a2581dc57e742",
+    "url": "/trackify/static/js/runtime-main.e7bcb764.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/trackify/static/media/logo.5d5d9eef.svg"
+  }
+]);
