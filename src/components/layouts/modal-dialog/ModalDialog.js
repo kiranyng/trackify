de944960f276d9274bfde366cccc81473633ee96
KiranYNG <kiran.yng@gmail.com>
2022-07-02 06:40:12 +0530

    modal dialog UI FX

--- src/components/layouts/modal-dialog/ModalDialog.js
+++ src/components/layouts/modal-dialog/ModalDialog.js
@@ -4,10 +4,16 @@ import ReactDOM from "react-dom";
 import "./ModalDialog.css";
 
 // TODO do this in a better way
-const dimRoot = () => document.getElementById('root').classList.add('dimmed');
-const resetRoot = () => document.getElementById('root').classList.remove('dimmed');
+const dimRoot = () => { 
+    document.getElementById('root').classList.add('dimmed'); 
+    document.getElementById('modal-root').classList.add('showed'); 
+};
+const resetRoot = () => { 
+    document.getElementById('root').classList.remove('dimmed'); 
+    document.getElementById('modal-root').classList.remove('showed'); 
+}
 
-const ModalDialog = forwardRef( (props, ref) => {
+const ModalDialog = forwardRef((props, ref) => {
     const [display, setDisplay] = React.useState(false);
 
     const open = () => {
@@ -29,23 +35,23 @@ const ModalDialog = forwardRef( (props, ref) => {
         };
     });
 
-    if(display){
+    if (display) {
         // TODO handle multiple JSX props to use as slots
-        return ReactDOM.createPortal (
+        return ReactDOM.createPortal(
             <div className="Modal-dialog" role="dialog">
-                <div onClick={ ref.current.closeModal } className="Modal-overlay"></div>
+                <div onClick={ref.current.closeModal} className="Modal-overlay"></div>
                 <div className="Modal-box">
-                    { props.title ? (<div className="Modal-title"> {props.title} </div>) : '' }
+                    {props.title ? (<div className="Modal-title"> {props.title} </div>) : ''}
 
                     <div className="Modal-content">
                         {props.children}
                     </div>
                 </div>
             </div>
-        , document.getElementById('modal-root'));
+            , document.getElementById('modal-root'));
     }
- 
+
     return null
-} );
+});
 
 export default ModalDialog;
