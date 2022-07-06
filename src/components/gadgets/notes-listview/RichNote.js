bcebbaa9dd2f39ca323afa96ef70561e0040a249
KiranYNG <kiran.yng@gmail.com>
2022-07-07 01:21:02 +0530

    note - supporting additional colors

--- src/components/gadgets/notes-listview/RichNote.js
+++ src/components/gadgets/notes-listview/RichNote.js
@@ -79,15 +79,22 @@ class RichNote extends React.Component {
               <EditButton cmd="fontsize" arg="16px" name="16" />
               <EditButton cmd="fontsize" arg="18px" name="18" />
 
-              <ColorButton cmd="ForeColor" color="white"/>
-              <ColorButton cmd="ForeColor" color="black"/>
-              <ColorButton cmd="ForeColor" color="#452977"/>
-              <ColorButton cmd="ForeColor" color="#2ce82c"/>
-              <ColorButton cmd="ForeColor" color="#427be6"/>
-              <ColorButton cmd="ForeColor" color="#d24bd2"/>
-              <ColorButton cmd="ForeColor" color="#1bca58"/>
-              <ColorButton cmd="ForeColor" color="orange"/>
-              <ColorButton cmd="ForeColor" color="red"/>
+              <ColorButton cmd="BackColor" color="white"/>
+              <ColorButton cmd="BackColor" color="black"/>
+              <ColorButton className="transparent-bg" cmd="BackColor" color="initial"/>
+              <ColorButton cmd="BackColor" color="yellow"/>
+              <ColorButton cmd="BackColor" color="lawngreen"/>
+              <ColorButton cmd="BackColor" color="cyan"/>
+              <ColorButton cmd="BackColor" color="pink"/>
+              <ColorButton cmd="BackColor" color="blue"/>
+              <ColorButton cmd="BackColor" color="red"/>
+              <ColorButton cmd="BackColor" color="darkblue"/>
+              <ColorButton cmd="BackColor" color="teal"/>
+              <ColorButton cmd="BackColor" color="green"/>
+              <ColorButton cmd="BackColor" color="violet"/>
+              <ColorButton cmd="BackColor" color="darkred"/>
+              <ColorButton cmd="BackColor" color="lightgray"/>
+              <ColorButton cmd="BackColor" color="darkgray"/>
             </div>
             <div className="Note-view-controls-colors">
               <EditButton cmd="bold" name="B" />
@@ -108,15 +115,22 @@ class RichNote extends React.Component {
                 <EditButton cmd="fontsize" arg="26px" name="26" />
                 <EditButton cmd="fontsize" arg="28px" name="28" />
 
-                <ColorButton cmd="BackColor" color="white"/>
-                <ColorButton cmd="BackColor" color="black"/>
-                <ColorButton cmd="BackColor" color="rgb(189 153 255)"/>
-                <ColorButton cmd="BackColor" color="#2ce82c"/>
-                <ColorButton cmd="BackColor" color="#427be6"/>
-                <ColorButton cmd="BackColor" color="#d24bd2"/>
-                <ColorButton cmd="BackColor" color="#1bca58"/>
-                <ColorButton cmd="BackColor" color="orange"/>
-                <ColorButton cmd="BackColor" color="red"/>
+                <ColorButton cmd="ForeColor" color="white"/>
+                <ColorButton cmd="ForeColor" color="black"/>
+                <ColorButton className="transparent-bg" cmd="ForeColor" color="initial"/>
+                <ColorButton cmd="ForeColor" color="darkred"/>
+                <ColorButton cmd="ForeColor" color="orange"/>
+                <ColorButton cmd="ForeColor" color="yellow"/>
+                <ColorButton cmd="ForeColor" color="lightgreen"/>
+                <ColorButton cmd="ForeColor" color="green"/>
+                <ColorButton cmd="ForeColor" color="lightblue"/>
+                <ColorButton cmd="ForeColor" color="blue"/>
+                <ColorButton cmd="ForeColor" color="darkblue"/>
+                <ColorButton cmd="ForeColor" color="purple"/>
+                <ColorButton cmd="ForeColor" color="orange"/>
+                <ColorButton cmd="ForeColor" color="lightgray"/>
+                <ColorButton cmd="ForeColor" color="gray"/>
+                <ColorButton cmd="ForeColor" color="darkgray"/>
             </div>
           </div>
           <ContentEditable
@@ -166,14 +180,17 @@ class RichNote extends React.Component {
   }
   
   function ColorButton(props) {
+    const style = { backgroundColor: (props.color === 'initial' ? 'transparent' : props.color) };
+    const className = 'color-button'+(props.color === 'initial' ? ' transparent-bg' : '')
+
     return (
-      <button className='color-button' style={ { backgroundColor: props.color } }
+      <button className={ className } style={ style }
         key={props.cmd}
         onMouseDown={evt => {
           evt.preventDefault(); // Avoids loosing focus from the editable area
-          document.execCommand(props.cmd, false, props.color); // Send the command to the browser
 
-          // document.execCommand(‘ForeColor’, false, ‘red’);
+          document.execCommand("styleWithCSS", 0, true);
+          document.execCommand(props.cmd, false, props.color); // Send the command to the browser
         }}
       >
         &nbsp;
